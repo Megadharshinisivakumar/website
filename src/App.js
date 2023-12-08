a83cede911c547331baf537942dcc4d5cbdc0890
Megadharshinisivakumar <megadharshinisivakumar@gmail.com>
2023-12-08 19:02:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Name:Megadharshini S</h1>
+      <h1>Department:BTech Information Technology</h1>
+      <h1>Year:3rd</h1>
+      <h1>Register Number:73772121162</h1>
+      <h1>Area of Interest:Full Stack Web Development</h1>
+    </div>
+  );
+}
+
+export default App;
